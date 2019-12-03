b7c8d96263328e594180a28d386a309c0ba5aece
David Pomerenke <david.pomerenke@uni-konstanz.de>
2019-12-03 16:34:36 +0000

    change demo parameter

--- docs/index.js
+++ docs/index.js
@@ -9,6 +9,6 @@ select.onchange = () => {
   const match = text.match(/^lang=([a-z]+)/)
   const lang = match && ['en', 'de'].includes(match[1]) ? match[1] : 'en'
   if (match) text = text.slice(match[0].length + 1)
-  window.alphabetify(text, alphabet, lang, 0.2, 0.2)
+  window.alphabetify(text, alphabet, lang, 0, 0.2)
     .then(result => (target.value += result))
 }
