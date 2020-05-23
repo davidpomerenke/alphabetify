c99559228322abe55b522d3c1be724b9fe23735e
David Pomerenke <david.pomerenke@uni-konstanz.de>
2020-05-24 00:57:13 +0200

    Trigger on textarea input

--- docs/index.js
+++ docs/index.js
@@ -1,14 +1,23 @@
-const buildDir = 'https://raw.githubusercontent.com/davidpomerenke/alphabetify/master/alphabets/build'
+const buildDir =
+  'https://raw.githubusercontent.com/davidpomerenke/alphabetify/master/alphabets/build'
 const select = document.getElementById('alphabet')
 const source = window.document.getElementById('source')
 const target = document.getElementById('target')
-select.onchange = () => {
-  target.value = ''
+const load = () => {
   const alphabetCode = select.options[select.selectedIndex].value
-  fetch(`${buildDir}/${document.documentElement.lang}/${alphabetCode}.json`)
-    .then(response =>
-      response.json()
-        .then(result =>
-          target.value += transform(source.value, result, 0, 0.2)))
-    .catch(e => console.log(e))
+  // do nothing if no alphabet is selected
+  if (!alphabetCode.match(/\s/)) {
+    target.value = 'Loading ...\nIf this takes too long, try a shorter text.'
+    fetch(`${buildDir}/${document.documentElement.lang}/${alphabetCode}.json`)
+      .then(response =>
+        response
+          .json()
+          .then(
+            result => (target.value = transform(source.value, result, 0, 0.2))
+          )
+      )
+      .catch(e => console.log(e))
+  }
 }
+select.onchange = load
+source.oninput = load
